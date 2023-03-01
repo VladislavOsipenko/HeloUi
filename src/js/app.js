6b1c9feb28bbec7c044b594680a71cca35cd8857
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-03-01 21:00:52 +0200

    start animation

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from "swiper";
 
+
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.main__swiper', {
         modules: [Navigation, Pagination],
@@ -22,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         breakpoints: {
             992: {
-                slidesPerView: 5,
+                slidesPerView: 6,
             },
             768: {
                 slidesPerView: 4,
@@ -53,6 +54,104 @@ document.addEventListener('DOMContentLoaded', () => {
                 menu.classList.toggle('header__menu_active');
             })
         })
+
+    $('ul.main__buttons').on('click', 'li:not(.main__btn-active)', function() {
+        $(this)
+            .addClass('main__btn-active').siblings().removeClass('main__btn-active')
+            .closest('div.container').find('div.main__pictures').removeClass('main__pictures-active').eq($(this).index()).addClass('main__pictures-active');
+    });
+
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Please specify your name",
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                }
+            },
+            errorClass: 'validator',
+        });
+    }   
+
+    validateForms('#footer-form');
+    validateForms('#modal__form');
+    validateForms('#modal__forma');
+
+
+
+    $('[data-modal=modal]').on('click', function() {
+        $('.overlay, #modal').fadeIn('slow');
+    });
+
+    $('[data-signin=signin]').on('click', function() {
+        $('.overlay, #signin').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #modal, #thanks, #signin').fadeOut('slow');
+    });
+
+
+    $(document).ready(function () {
+        $('.footer__item-title').click(function (event) {
+            if($('.footer').hasClass('one')) {
+                $('.footer__item-title').not($(this)).removeClass('.active');
+                $('.footer__item-wrapper').not($(this).next()).slideUp(300);
+
+            }
+            $(this).toggleClass('active').next().slideToggle(300);
+        });
+    });
+
+    const animItems = document.querySelectorAll('._anim-items');
+
+    if(animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart; 
+                }
+
+                if ((scrollY > animItemOffset - animItemPoint) && scrollY <(animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+                    
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(), 
+                scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+                scrollTop = window.scrollY || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 400);
+        
+
+    }
 });
 
 
